4a07318243e3ccf1f9a1ed4b75b7cc15dd263993
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-01-21 19:51:38 -0800

    Added structure for unit testing

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+/*jshint browser:false, node:true */
+
+// Karma test runner configuration
+// see http://karma-runner.github.io/0.12/config/configuration-file.html
+
+module.exports = function(config) {
+  "use strict";
+
+  config.set({
+    frameworks: [ "qunit", "sinon" ],
+    files: [
+      "tests/*.test.js"
+    ],
+    browsers: [ "PhantomJS" ],
+    reporters: [ process.env.CI ? "dots" : "progress", "coverage" ],
+    preprocessors: {
+      "src/hubspot-oauth-client.js": [ "coverage" ]
+    },
+    coverageReporter: {
+      dir: "tests/coverage",
+      reporters: [
+        { type: "html", subdir: "html" },
+        { type: "lcovonly", subdir: ".", file: "report.lcov" },
+        { type: "text", subdir: ".", file: "report.txt" },
+        { type: "text-summary", subdir: ".", file: "summary.txt" }
+      ]
+    },
+    customLaunchers: {
+      "PhantomJS_debug": {
+        base: "PhantomJS",
+        options: {
+          windowName: "Custom PhantomJS",
+          settings: {
+            webSecurityEnabled: false
+          }
+        },
+        flags: [
+          "--remote-debugger-port=9000",
+          "--remote-debugger-autorun=yes"
+        ]
+      }
+    }
+  });
+};
---
+++ tests/hubspot-oauth-client.test.js
@@ -0,0 +1,51 @@
+(function() {
+
+  QUnit.module("HubSpotOAuthClient");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+  QUnit.module("_hasPendingOAuthIntegration");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+  QUnit.module("_setHubId");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+  QUnit.module("oAuthCallback");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+  QUnit.module("_getOAuthURL");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+  QUnit.module("_getWindowTitle");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+  QUnit.module("_getWindowConfig");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+  QUnit.module("initiateOAuthIntegration");
+
+  QUnit.test("NOT IMPLEMENTED", function(assert) {
+    assert.expect(0);
+  });
+
+})();
